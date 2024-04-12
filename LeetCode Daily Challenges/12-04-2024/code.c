a77a867fac34a8e59f0f4d544b3e034a817aa0d4
petchi11 <petchima08@gmail.com>
2024-04-12 22:54:54 +0530

    new

---
+++ LeetCode Daily Challenges/12-04-2024/code.c
@@ -0,0 +1,38 @@
+// int max ( int , int ); 
+int trap(int* height, int heightSize) {
+    int start;
+    int end;
+    int count;
+    int maximum;
+
+    start = 0;
+    end = heightSize - 1;
+
+    maximum = count = 0;
+
+    while ( start < end )
+    {
+        if ( *(height+start) <= *(height+end) )
+        {
+            //maximum = max (maximum, *(height+start));
+            maximum = ( maximum < *(height+start) ) ? *(height+start) : maximum;
+            count += maximum - *(height+start);
+            start += 1;
+        }
+        else
+        {
+            //maximum = max ( maximum, *(height+end) );
+            maximum = ( maximum < *(height+end) ) ? *(height+end) : maximum;
+            count += maximum - *(height+end);
+            end -= 1;
+        }
+    }
+
+    return count;
+}
+// int max ( int a, int b )
+// {
+//     if ( a > b )
+//         return a;
+//     return b;
+// }
